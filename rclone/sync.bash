85e07cd6ee0425b3fba145674cd4a3761c5ee3cf
Sidhant Unnithan <contact@sidhantunnithan.com>
2025-05-08 14:52:31 +0530

    added ports to env variable

--- rclone/sync.bash
+++ rclone/sync.bash
@@ -3,4 +3,8 @@ docker run -it --rm \
     -v /home/jindan/mounts:/sync \
     rclone/rclone \
     sync /sync b2:homelab-zeus \
+    --exclude "anime/**" \
+    --exclude "movies/**" \
+    --exclude "series/**" \
+    --exclude "downloads/**" \
     --progress --transfers 32
